24dd0de297db4ad75daa7e1aca989ac3460f9d0c
Thabo <ngubanethabo.ambrose@gmail.com>
2020-09-26 21:20:25 +0200

    WIP

--- src/scss-migrate/index.ts
+++ src/scss-migrate/index.ts
@@ -1,10 +1,25 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { Rule, SchematicContext, Tree, url, apply, template, mergeWith } from '@angular-devkit/schematics';
+import { Schema } from './schema';
+import { strings } from '@angular-devkit/core';
 
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
-export function scssMigrate(_options: any): Rule {
+export function scssMigrate(_options: Schema): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+    const { name } = _options;
+
+    const sourceTemplates = url('./');
+
+    const sourceParametrizedTemplate = apply(sourceTemplates, [
+      template({
+        ..._options,
+        ...strings
+      })
+    ])
+
+    tree.create('hello.js', `console.log('hello ${name}!')`);
+
+    return mergeWith(sourceParametrizedTemplate)(tree, _context);
   };
 }
